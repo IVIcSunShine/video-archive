51aba28d161c61e4513114ba9e044f3f777067c7
Peter Ruhland <ruhland.peter@gmx.net>
2023-05-28 11:08:00 +0200

    some changes in class VideoController

--- demo/src/main/java/video/VideoController.java
+++ demo/src/main/java/video/VideoController.java
@@ -1,8 +1,21 @@
 package video;
 
 import org.springframework.web.bind.annotation.RestController;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 public class VideoController {
     
+    private List<Video> videolist = new ArrayList<Video>(Arrays.asList(
+        new Video("Edge of Tomorrow", "16", "In Zeitschleife gefangen bis Aliens vernichtet", "SciFi"),
+        new Video("Security", "18", "Marine rettet Mädchen in Kaufhaus", "Action")
+    ));
+
+    @RequestMapping("/videos")
+    public List<Video> getVideoList(){
+        return videolist;
+    }
 }
