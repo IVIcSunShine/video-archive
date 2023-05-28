22328dad8d6cccba79fe68b413c9baf067cda658
Peter Ruhland <ruhland.peter@gmx.net>
2023-05-28 15:21:45 +0200

    separated Service class from Controller class

--- demo/src/main/java/com/example/demo/video/VideoController.java
+++ demo/src/main/java/com/example/demo/video/VideoController.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,54 +16,32 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @RestController
 public class VideoController {
-    
-    private List<Video> videolist = new ArrayList<Video>(Arrays.asList(
-        new Video("Edge of Tomorrow", "16", "In Zeitschleife gefangen bis Aliens vernichtet", "SciFi"),
-        new Video("Security", "18", "Marine rettet Mädchen in Kaufhaus", "Action")
-    ));
 
-    @RequestMapping("/videos")
-    public List<Video> getVideoList(){
-        return videolist;
-    }
+@Autowired
+VideoService videoService;
 
-    @RequestMapping("/videos/{title}")
-    public Video getVideo(@PathVariable String title){
-        for (Video v:videolist){
-            if(v.getTitle().equals(title))
-            return v;
-        }
-        return null;
-    }
-    
-    @RequestMapping(method=RequestMethod.POST, value="/videos")
-    public void addVideo(@RequestBody Video video){
-        videolist.add(video);
-    }
+@RequestMapping("/videos")
+public List<Video> getVideoList() {
+return videoService.getVideoList();
+}
 
-    @RequestMapping(method=RequestMethod.PUT, value="/videos/{title}")
-    public void updateVideo(@PathVariable String title, @RequestBody Video video){
-        for(int i=0; i<videolist.size(); i++){
-            Video v = videolist.get(i);
-            if(v.getTitle().equals(title)){
-                videolist.set(i,video);
-                return;
-            }
-        }
-    }
+@RequestMapping("/videos/{title}")
+public Video getVideo(@PathVariable String title) {
+return videoService.getVideo(title);
+}
 
+@RequestMapping(method=RequestMethod.POST, value="/videos")
+public void addVideo(@RequestBody Video video) {
+videoService.addVideo(video);
+}
 
+@RequestMapping(method=RequestMethod.PUT, value="/videos/{title}")
+public void updateVideo(@PathVariable String title, @RequestBody Video video) {
+videoService.updateVideo(title, video);
+}
 
-   
-
-    @RequestMapping(method=RequestMethod.DELETE, value="/videos/{title}")
-    public void deleteVideo(@PathVariable String title){
-        for(int i=0; i<videolist.size(); i++){
-            Video v = videolist.get(i);
-            if(v.getTitle().equals(title)){
-                videolist.remove(i);
-                return;
-            }
-        }
-    }
+@RequestMapping(method=RequestMethod.DELETE, value="/videos/{title}")
+public void deleteVideo(@PathVariable String title) {
+videoService.deleteVideo(title);
+}
 }
